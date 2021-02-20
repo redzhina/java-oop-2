470d83fdf027520fec3332599c1f1b0a48002b83
Regina Shakirova <regina.shak@yandex.ru>
2021-02-21 00:35:43 +0300

    vk1

---
+++ src/main/java/domain/post.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class post {
+    private int id;
+    private String communityName;
+    private String text;
+    private String imageUrl;
+    private int likesNumber;
+    private int commentsNumber;
+    private int repostsNumber;
+    private String comments;
+    private int viewsNumber;
+    private String dateOfPosting;
+    private String postedByName;
+
+}
